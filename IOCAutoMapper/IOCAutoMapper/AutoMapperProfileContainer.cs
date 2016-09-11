1199202ca07f74fccb00df673d8dd1e6da43368b
dvkudr <dvkudr@gmail.com>
2016-09-11 20:06:45 +0300

    improvements

--- IOCAutoMapper/IOCAutoMapper/AutoMapperProfileContainer.cs
+++ IOCAutoMapper/IOCAutoMapper/AutoMapperProfileContainer.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 
 namespace IOCAutoMapper
 {
     public class AutoMapperProfileContainer<T>
     {
-        public AutoMapperProfileContainer(Profile[] profiles)
+        public AutoMapperProfileContainer(IList<Profile> profiles)
         {
             Console.WriteLine($"constructor {nameof(AutoMapperProfileContainer<T>)} for {typeof(T).Name}");
             Profiles = new ReadOnlyCollection<Profile>(profiles);
--- IOCAutoMapper/IOCAutoMapper/Program.cs
+++ IOCAutoMapper/IOCAutoMapper/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.Practices.Unity;
 
@@ -12,14 +8,24 @@ namespace IOCAutoMapper
     {
         static void Main(string[] args)
         {
-            var p1 = new Profile1();
-            var p2 = new Profile2();
+            Extensions.Container.RegisterType<Profile1>(new ContainerControlledLifetimeManager());
+            Extensions.Container.RegisterType<Profile2>(new ContainerControlledLifetimeManager());
 
-            Extensions.Container.RegisterType<AutoMapperProfileContainer<Consumer1>>(
-                new ContainerControlledLifetimeManager(), new InjectionConstructor(new object[] {new Profile[]{ p1, p2 }}));
+            Extensions.Container.RegisterType<AutoMapperProfileContainer<Consumer1>>(new ContainerControlledLifetimeManager(),
+                new InjectionConstructor(new object[]
+                {
+                    new Profile[]
+                    {
+                        Extensions.Container.Resolve<Profile1>(),
+                        Extensions.Container.Resolve<Profile2>()
+                    }
+                }));
 
-            Extensions.Container.RegisterType<AutoMapperProfileContainer<Consumer2>>(
-                new ContainerControlledLifetimeManager(), new InjectionConstructor(new object[] { new Profile[] { p1 } }));
+            Extensions.Container.RegisterType<AutoMapperProfileContainer<Consumer2>>(new ContainerControlledLifetimeManager(),
+                new InjectionConstructor(new object[]
+                {
+                    new Profile[] { Extensions.Container.Resolve<Profile2>() }
+                }));
 
             Extensions.Container.RegisterType<AutoMapperFactory>(new ContainerControlledLifetimeManager());
 
